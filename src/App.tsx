d4c43bac45ace933b78452073fa2c4f454f7bc1c
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-08-04 02:38:30 +0300

    feat: add basic images loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
-import { Component, createSignal } from "solid-js";
-import "./App.scss";
-import Counter from "./Counter";
+import { Component, createSignal, For } from "solid-js";
+import { dialog, fs, tauri } from "@tauri-apps/api";
+import { FileEntry } from "@tauri-apps/api/fs";
+
+const extractFiles = (fileEntry: FileEntry) => {
+  return fileEntry.children
+    ? fileEntry.children.map(extractFiles)
+    : tauri.convertFileSrc(fileEntry.path);
+};
 
 const App: Component = () => {
-  const [counter, setCounter] = createSignal(0);
-  setInterval(setCounter, 1000, (c: number) => c + 1);
+  const [files, setFiles] = createSignal<string[]>([]);
+
+  const load = async () => {
+    const dir = (await dialog.open({
+      directory: true,
+    })) as string;
+
+    const data = await fs.readDir(dir, {
+      recursive: true,
+    });
+
+    const pathes = data.map(extractFiles).flat(Infinity);
+    setFiles(pathes);
+  };
 
   return (
-    <>
-      <div>
-        <h1 class="header">{counter()}</h1>
-      </div>
-      <Counter />
-    </>
+    <div>
+      <button onClick={load}>load</button>
+
+      <For each={files()}>{(file) => <img src={file} loading="lazy" />}</For>
+    </div>
   );
 };
 
